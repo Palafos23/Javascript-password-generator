c2e87811921060b1797de12ff37ba5e5e671633f
Jp <josephpalafos24@gmail.com>
2023-06-17 21:56:48 -0500

    added var and pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//var lowerCase = ("");
+//var upperCase = ("");
+//var number = ("");
+//var SpecialCharcaters = ("");
 
+function generatePassword(){
+  console.log("clicked button");
 
+//prompt user with criteria
+   //-length of 8 < 120 characters 
+   //-choose to include lowercase,uppercase, numeric or special characters
+//-input validated and atleast one character type to be displayed 
+   //-password matches criteria
+//displayed in alert or written on page
+  return "password here";
+}
 
 // Write password to the #password input
 function writePassword() {
