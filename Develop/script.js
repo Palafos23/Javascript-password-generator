7aee5b81999c2655319621057de6f1f43a9aa534
Jp <josephpalafos24@gmail.com>
2023-06-20 18:09:09 -0500

    updated javascript

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,8 @@ var generateBtn = document.querySelector("#generate");
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "?"];
-  var allCharacters = [lowerCase, upperCase, number, specialCharacters];
   var allCharacterss = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "?"];
-
+  
   function generatePassword() {
 
     password = "";
@@ -20,7 +19,34 @@ var generateBtn = document.querySelector("#generate");
     window.alert("Character length has to be more than 8 but less than 128.");
     return false;
     }
-    //random for one variable that contains all characters 
+    
+    //prompts below 
+    for(var i = 0; i < userChoicelenght; i++) {
+    var randomCharacters = Math.floor(Math.random() *characterArray.length);
+    var password = password + characterArray[randomCharacters];
+    }
+   
+    if (window.confirm("Do you want lowercase letters?")) {
+    characterArray = characterArray.concat(lowerCase);
+    }else {
+      
+      window.alert("no lowercase.");
+    }
+
+    if (window.confirm("Do you want uppercase letters?")) {
+    characterArray = characterArray.concat(upperCase);
+    }
+
+    /*if (window.confirm("Do you want numbers?")) {
+      characterArray = characterArray.concat(number);
+    }
+
+    if (window.confirm("Do you want special characters?")) {
+    characterArray = characterArray.concat(specialCharacters);
+    }*/
+    return password;
+ }
+//random for one variable that contains all characters 
 
     /*for(var i = 0; i < userChoicelenght; i++) {
     var allChars = Math.floor(Math.random() *allCharacterss.length);
@@ -34,58 +60,13 @@ var generateBtn = document.querySelector("#generate");
     return password;
     }*/
 
-    //random for one variable 
-
-    /*for(var i = 0; i < userChoicelenght; i++) {
-    var randomLower = Math.floor(Math.random() *lowerCase.length);
-    var password = password + lowerCase[randomLower]
-    }
-
-   if (window.confirm("Do you want lowercase letters?")) {
-      characterArray = characterArray.concat(lowerCase);
-      window.alert(password);
-
-    }*/
-   
-    //prompts below 
-    for(var i = 0; i < userChoicelenght; i++) {
-    var randomCharacters = Math.floor(Math.random() *characterArray.length);
-    var password = password + characterArray[randomCharacters];
-    } 
-
-    if (window.confirm("Do you want lowercase letters?")) {
-    characterArray = characterArray.concat(lowerCase);
-    }else {
-     lowerCase = "";
-    }
-
-    if (window.confirm("Do you want uppercase letters?")) {
-    characterArray = characterArray.concat(upperCase);
-    }else {
-      upperCase = ""
-    }
-
-    if (window.confirm("Do you want numbers?")) {
-      characterArray = characterArray.concat(number);
-
-    }
-
-    if (window.confirm("Do you want special characters?")) {
-    characterArray = characterArray.concat(specialCharacters);
-    }
-    return password;
- }
-
-// Write password to the #password input
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-if (password) {
+  if (password) {
   passwordText.value = password;
 }
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
